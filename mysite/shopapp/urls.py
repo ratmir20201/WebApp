daf4309b9ceb524bd9acfa204972f20c838ecec9
ratmir <ratmir2020.1@gmail.com>
2024-10-15 11:42:53 +0500

    добавил проект

---
+++ mysite/shopapp/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+
+from shopapp.views import (
+    shop_index,
+    user_room,
+    product_create,
+)
+
+app_name = "shopapp"
+
+urlpatterns = [
+    path("", shop_index, name="index"),
+    path("user-room/", user_room, name="user_room"),
+    path("product-create/", product_create, name="product_create"),
+]
---
+++ mysite/shopapp/views.py
@@ -0,0 +1,17 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def shop_index(request):
+    return render(request, 'shopapp/index.html')
+
+def user_room(request):
+    context = {
+        "name": "Пётр",
+        "email": "some@gmail.com",
+        "phone": 87779998888
+    }
+    return render(request, 'shopapp/user_room.html', context=context)
+
+def product_create(request):
+    return render(request, 'shopapp/product-create.html')
