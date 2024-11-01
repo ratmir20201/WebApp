8629cccd66a9826e6237e3785630b8b257abe245
ratmir <ratmir2020.1@gmail.com>
2024-11-01 17:36:38 +0500

    Добавил модель Book и сделал страничку со списком всех книг

--- mysite/shopapp/views.py
+++ mysite/shopapp/views.py
@@ -1,11 +1,13 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpRequest
 from django.shortcuts import render
 
+from shopapp.models import Book
 
-def shop_index(request):
+
+def shop_index(request:  HttpRequest) -> HttpResponse:
     return render(request, 'shopapp/index.html')
 
-def user_room(request):
+def user_room(request:  HttpRequest) -> HttpResponse:
     context = {
         "name": "Пётр",
         "email": "some@gmail.com",
@@ -13,5 +15,14 @@ def user_room(request):
     }
     return render(request, 'shopapp/user_room.html', context=context)
 
-def product_create(request):
+def product_create(request:  HttpRequest):
     return render(request, 'shopapp/product-create.html')
+
+def book_list(request: HttpRequest) -> HttpResponse:
+    books = Book.objects.all()
+    total_books = books.count()
+    context = {
+        'books': books,
+        'total_books': total_books
+    }
+    return render(request, 'shopapp/book_list.html', context=context)
