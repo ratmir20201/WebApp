319c03a1825360725ffdf2240b430f0c2f97a0f0
ratmir <ratmir2020.1@gmail.com>
2024-11-04 16:33:04 +0500

    добавил модели Product и Order а также создал ProductAdmin для взаимодействия через админку

--- mysite/shopapp/admin.py
+++ mysite/shopapp/admin.py
@@ -1,9 +1,17 @@
 from django.contrib import admin
-from .models import Book
+from .models import Book, Product, Order
 
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'published_date', 'isbn')
-    list_filter = ('author',)
+    list_filter = ('author', )
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'pk', 'name', 'short_description', 'price', 'discount', 'created_at'
+    )
+    list_display_links = ('name', )
+    list_filter = ('name', )
 
 admin.site.register(Book, BookAdmin)
--- mysite/shopapp/models.py
+++ mysite/shopapp/models.py
@@ -1,3 +1,4 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 class Book(models.Model):
@@ -5,3 +6,30 @@ class Book(models.Model):
     author = models.CharField(max_length=100)
     published_date = models.DateField()
     isbn = models.CharField(max_length=13)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, null=False)
+    description = models.TextField(null=False, blank=True)
+    price = models.IntegerField(default=0)
+    discount = models.SmallIntegerField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def short_description(self):
+        if len(self.description) < 50:
+            return self.description
+        return self.description[:50] + "..."
+
+    def __str__(self):
+        return "Продукт: {product}".format(
+            product=self.name
+        )
+
+
+class Order(models.Model):
+    delivery_address = models.TextField(null=False, blank=False)
+    promocode = models.CharField(max_length=20, null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    user = models.ForeignKey(User, on_delete=models.PROTECT)
+    products = models.ManyToManyField(Product, related_name="orders")
